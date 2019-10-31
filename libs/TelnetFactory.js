2678d88bb1ef5ef0e7567bfa2a0af1bd19c026bb
315920958@qq.com <315920958@qq.com>
2019-10-31 11:05:17 +0800

    telnet tools

---
+++ libs/TelnetFactory.js
@@ -0,0 +1,42 @@
+const factory = {};
+module.exports = factory;
+factory.create = function (port,messageHandler)
+{
+    //telnet
+    const telnet = {};
+    return new Promise((res,rej)=>{
+        const server = net.createServer((con) => {
+            con.write('success\n');
+            telnet.server = server;
+
+            con.on('data',async data=>{
+                let str = data.toString().trim();
+                if(!str)
+                {
+                    con.write('dataError!\n');
+                    return;
+                }
+
+                let paras = str.split(/\s+/gi)
+                let key = paras.shift();
+
+                if(key === 'quit'){
+                    console.log('close')
+                    con.end()
+                    return
+                }
+
+                var result = messageHandler && await messageHandler(key,...paras) || null;
+                result ? con.write(`${result}\n`) : con.write(`errror\n`);
+
+            })
+
+            con.on('end', ()=>{
+                console.log('userclose')
+            });
+            res(telnet);
+        });
+        server.listen(port);
+
+    });
+}
