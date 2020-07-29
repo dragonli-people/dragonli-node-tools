1858b47257862dd136bd6764416fd78520d1513e
315920958@qq.com <315920958@qq.com>
2020-07-29 12:18:55 +0800

    去除日志 & bug fix

--- thridpart/upyun/UpyunStorageHandler.js
+++ thridpart/upyun/UpyunStorageHandler.js
@@ -1,4 +1,4 @@
-const upyun = require('pyun');
+const upyun = require('upyun');
 const {imgTypeFromBuffer} = require('../../libs/GeneralUtil');
 module.exports = class {
     constructor(bucket,operatorName,passwd){
@@ -10,15 +10,14 @@ module.exports = class {
         return await this.client.putFile(file,buffer);
     }
 
-    async putImageAndRename(buffer,prefix=''){
-        var type = imageBufferHeaders.find(item=>Buffer.from(item.bufBegin).equals(buffer.slice(0,item.bufBegin.length)));
-        type = type && type.suffix || null;
+    async putImageAndRename(buffer,prefix='',hostKey){
+        var type = imgTypeFromBuffer(buffer);
         if(!type) throw new Error('unknow imgae type in buffer content')
         prefix.match(/\/$/gi) || ( prefix = `${prefix}/` );
         var now = new Date();
 
         var filename = `${now.getTime()}-${1000000 + Math.floor(Math.random()*(1000000-1))}`;
-        filename = `${prefix}${now.format( 'yyyy' )}/${now.format( 'MM' )}/${now.format( 'dd' )}/${filename}`
-        return this.put( filename,buffer )
+        filename = `${prefix}${now.format( 'yyyy' )}/${now.format( 'MM' )}/${now.format( 'dd' )}/${filename}${type}`
+        return { url:filename,hostKey , result:await this.put( filename,buffer ) };
     }
 };
