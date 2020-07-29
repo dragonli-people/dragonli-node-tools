bc3fd4aee0a6abbbb47987277ca7ae5eceffe3cd
315920958@qq.com <315920958@qq.com>
2020-07-29 11:57:26 +0800

    去除日志

--- libs/ImageTagExtract.js
+++ libs/ImageTagExtract.js
@@ -4,10 +4,7 @@ async function handleImgTagInHtml(code,imgHandler,autoAddHostKey=false){
     var imgCodes = extractImgTagsInfo(code);
     code = code.replace( /<\/img\s+[^<>]*>/gi, '');//兼容</img>配对写法。只处理<img...>
     var list = code.split(/<img\s+[^<>]*>/gi);
-    console.log('list',list);
-    console.log('imgCodes',imgCodes);
     var imgCodetransforms = await Promise.all( imgCodes.map( v=>transformOneImgTag(v,imgHandler,autoAddHostKey) ) );
-    console.log('imgCodetransforms',imgCodetransforms);
     //list的长度必须比imgCodetransforms的长度大一，否则是不可思议之事
     var arr = imgCodetransforms.reduce((r,v,i)=>(r.push(list[i]),r.push(v),r),[]);
     arr.push( list.pop() );
@@ -29,6 +26,7 @@ async function extractOneImgInfo(str){
     var buffer,[_,code] = str.match(/src=\"(.*)\"/i) || [];
     if(!code)return null;
     var [__,base64] = code.match(/^data\:.*;base64,(.*)/i ) || [];
+    console.log('base64',base64 && base64.length || false);
     if(base64)return Buffer.from(base64,'base64');
     if( code.match(/^(http\:\/\/)|(https\:\/\/)/gi) ) return await httpGetBuffer(code);//远程抓取
     return null;
